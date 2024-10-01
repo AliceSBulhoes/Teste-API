4bdb96e5d9ee4522bd3e3e1f1d0e970d85e34d02
Alice <alicesb2006@gmail.com>
2024-10-01 08:10:09 -0300

    Criando a estrutura do Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+// Importando estilos
+import { NavStyle } from './NavStyle'
 
 const Nav = () => {
   return (
     <>
-    <header className='nav'>
-      <Link to='/' className='nav-link'>Home</Link>
-      <Link to='/cliente' className='nav-link'>Cliente</Link>
-    </header>
+    <NavStyle>
+      <header className='nav'>
+        <Link to='/' className='nav-link'>Home</Link>
+        <Link to='/cliente' className='nav-link'>Cliente</Link>
+        <Link to='/contato' className='nav-link'>Contato</Link>
+      </header>
+    </NavStyle>
     </>
   )
 }
