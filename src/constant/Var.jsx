2950bfd196eba50287578892b06de3ff221d4b1f
Alice <alicesb2006@gmail.com>
2024-10-01 08:10:42 -0300

    Criando constantes

--- src/constant/Var.jsx
+++ src/constant/Var.jsx
@@ -1,8 +1,9 @@
 
 export const Colors = {
     bg_color: '#FBF2ED',
-    hd_color: '#E8D4C6',
-    ft_color: '#B7BCBF',
+    hd_color: '#CDD5C6',
+    ft_color: '#80837d',
+    ft_color2: '#4b4c49',
     br_color1: '#FFE2C3',
     br_color2: '#E8D4C6'
 } 
@@ -10,5 +11,9 @@ export const Colors = {
 export const FontSize = {
     ft_text: '1rem',
     ft_title: '2rem',
-    ft_nav: '1.5rem'
+    ft_nav: '1.1rem'
+}
+
+export const FontMont = {
+    ft_family: "Montserrat, sans-serif"
 }
