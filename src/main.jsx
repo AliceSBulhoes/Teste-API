86fc722519f05e49d736d42769f812716bca2e1c
Alice <alicesb2006@gmail.com>
2024-10-01 08:11:02 -0300

    Criando a estrutura do main

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import App from './App.jsx'
 import Error from './routes/Error/Error.jsx'
 import Home from './routes/Home/Home.jsx'
 import Cliente from './routes/Cliente/Cliente.jsx'
+import { GlobalStyle } from './styles/GlobalStyle.jsx'
+import Contato from './routes/Contato/Contato.jsx'
 
 const router = createBrowserRouter([{
   path: '/',
@@ -14,12 +16,14 @@ const router = createBrowserRouter([{
 
   children: [
     {path: '/', element: <Home/>},
-    {path: '/cliente', element: <Cliente/> }
+    {path: '/cliente', element: <Cliente/> },
+    {path: '/contato', element: <Contato/>}
   ]
 }])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <GlobalStyle/>
   </StrictMode>,
 )
